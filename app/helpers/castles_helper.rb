bca4552173bd340991615c45d0a3453f2858e656
Vyacheslav Odinokov <vyacheslav.odinokov@simourg.com>
2023-12-12 12:12:07 +0000

    refactor: divided castle views to partials

--- app/helpers/castles_helper.rb
+++ app/helpers/castles_helper.rb
@@ -1,2 +1,24 @@
 module CastlesHelper
+  def button_for_build
+    content_tag(:button, 'Build', class: 'build-button')
+  end
+
+  def button_for_upgrade
+    content_tag(:button, 'Upgrade', class: 'upgrade-button')
+  end
+
+  def seconds_to_time(seconds)
+    return '0 seconds' if seconds.nil? || seconds.zero?
+
+    days, hours, minutes, remaining_seconds = seconds.divmod(24 * 3600)
+    hours, minutes, seconds = hours.divmod(3600), minutes.divmod(60), remaining_seconds.divmod(60)
+  
+    time_components = []
+    time_components << "#{days} days" if days > 0
+    time_components << "#{hours} hours" if hours > 0
+    time_components << "#{minutes} minutes" if minutes > 0
+    time_components << "#{seconds} seconds" if seconds > 0
+  
+    time_components.join(', ')
+  end
 end
--- app/models/building_type.rb
+++ app/models/building_type.rb
@@ -2,32 +2,36 @@
 
 class BuildingType
   TYPES = {
-    citadel: 1,
-    barracks: 2,
-    militia_hq: 3,
-    farm: 4,
-    sawmill: 5,
-    stone_mine: 6,
-    iron_mine: 7,
-    diplomatic_center: 8,
-    defensive_wall: 9
+    citadel: { id: 1, name: 'Citadel', details: 'Unlocks new possibilities' },
+    barracks: { id: 2, name: 'Barracks', details: 'Training new troops' },
+    militia_hq: { id: 3, name: 'Militia Headquarters', details: 'Studying and preparing new combat tactics' },
+    farm: { id: 4, name: 'Farm', details: 'Resource gathering: food' },
+    sawmill: { id: 5, name: 'Sawmill', details: 'Resource gathering: wood' },
+    stone_mine: { id: 6, name: 'Stone Mine', details: 'Resource gathering: stone' },
+    iron_mine: { id: 7, name: 'Iron Mine', details: 'Resource gathering: iron' },
+    diplomatic_center: { id: 8, name: 'Diplomatic Center', details: 'Join or create new alliance' },
+    defensive_wall: { id: 9, name: 'Defensive Wall', details: 'Enhances defense protection' }
   }.freeze
 
-  attr_reader :type_id
+  attr_reader :type
 
-  def initialize(type_id)
-    @type_id = type_id.to_sym
+  def initialize(type)
+    @type = type.to_sym
   end
 
   def name
-    TYPES.key(@type_id)
+    TYPES[@type][:name]
+  end
+
+  def details
+    TYPES[@type][:details]
   end
 
   def to_i
-    TYPES[@type_id]
+    TYPES[@type][:id]
   end
 
   def self.find_by_name(name)
-    new(TYPES[name.to_sym])
+    new(TYPES[name.to_sym][:id])
   end
 end
