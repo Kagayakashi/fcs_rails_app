d6d2ff123096ec7be61448f562f75f3c9796ad81
Vyacheslav Odinokov <vyacheslav.odinokov@simourg.com>
2023-12-12 15:26:07 +0000

    feature: added tailwind, added styles for resources info panel

---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}'
+    ],
+    theme: {
+        screens: {
+            sm: '240px',
+            md: '480px',
+            lg: '768px',
+            xl: '1366px',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ]
+}
