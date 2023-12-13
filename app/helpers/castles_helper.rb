4e0fdd7b94c6aa2bed3282480a7dfd28f3fe20d6
Vyacheslav Odinokov <vyacheslav.odinokov@simourg.com>
2023-12-13 07:57:01 +0000

    feature: updated styles for buttons and resources

--- app/helpers/castles_helper.rb
+++ app/helpers/castles_helper.rb
@@ -1,27 +1,33 @@
 module CastlesHelper
   def button_for_build(building)
-    button_to(building.level.zero? ? 'Build' : 'Upgrade', build_building_path(building), method: :post, class: 'build-button')
+    button_to(building.level.zero? ? 'Build' : 'Upgrade', build_building_path(building),
+        method: :post,
+        class: 'sm:w-[65px] sm:h-[25px] bg-green-400 border-solid border-2 border-indigo-600 rounded sm:text-sm')
   end
 
   def button_for_cancel(building)
-    button_to('Cancel', cancel_building_path(building), method: :post, class: 'cancel-button')
+    button_to('Cancel', cancel_building_path(building),
+        method: :post,
+        class: 'sm:w-[65px] sm:h-[25px] bg-red-400 border-solid border-2 border-indigo-600 rounded sm:text-sm')
   end
 
   def button_for_skip(building)
-    button_to('Skip', skip_building_path(building), method: :post, class: 'skip-button')
+    button_to('Skip', skip_building_path(building),
+        method: :post,
+        class: 'sm:w-[65px] sm:h-[25px] bg-yellow-400 border-solid border-2 border-indigo-600 rounded sm:text-sm')
   end
 
   def seconds_to_time(seconds)
-    return '0 seconds' if seconds.nil? || seconds.zero?
+    return '0s' if seconds.nil? || seconds.zero?
 
     days, hours, minutes, remaining_seconds = seconds.divmod(24 * 3600)
     hours, minutes, seconds = hours.divmod(3600), minutes.divmod(60), remaining_seconds.divmod(60)
   
     time_components = []
-    time_components << "#{days} days" if days > 0
-    time_components << "#{hours} hours" if hours > 0
-    time_components << "#{minutes} minutes" if minutes > 0
-    time_components << "#{seconds} seconds" if seconds > 0
+    time_components << "#{days}d" if days > 0
+    time_components << "#{hours}h" if hours > 0
+    time_components << "#{minutes}m" if minutes > 0
+    time_components << "#{seconds}s" if seconds > 0
   
     time_components.join(', ')
   end
